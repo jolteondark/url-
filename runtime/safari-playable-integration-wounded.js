1b8a02bad0b97cc821b0f933613989161baf50b6
jolteondark <somasoso0522@gmail.com>
2026-08-16 21:00:14 +0900

    Safari: isolate wounded layer from existing playable core

--- runtime/safari-playable-integration-wounded.js
+++ runtime/safari-playable-integration-wounded.js
@@ -1,11 +1,11 @@
-import * as base from "./safari-playable-integration-core.js";
+import * as base from "./safari-playable-integration-pre-wounded.js";
 import {
   prepareSafariWoundedPokemonCandidate,
   resolveSafariWoundedPokemonDecision,
   safariWoundedHealingInventory,
 } from "./safari-wounded-pokemon-integration.js";
 
-export * from "./safari-playable-integration-core.js";
+export * from "./safari-playable-integration-pre-wounded.js";
 export {
   prepareSafariWoundedPokemonCandidate,
   resolveSafariWoundedPokemonDecision,
