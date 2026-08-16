8677dcae7a70f7d0fa54cdbe3f4734a4cce7a1a6
jolteondark <somasoso0522@gmail.com>
2026-08-16 23:20:59 +0900

    Safari: export owner-routed Center activation

--- runtime/safari-playable-integration.js
+++ runtime/safari-playable-integration.js
@@ -1,2 +1,3 @@
 export * from "./safari-playable-integration-wounded.js";
+export { activateSafariDayBoardCell } from "./safari-pokemon-center-command.js";
 export { attemptSafariCapture } from "./safari-capture-command.js";
