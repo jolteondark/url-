4deb6fd9db8fb84ca8dd9b40c20f8340cca11ceb
jolteondark <somasoso0522@gmail.com>
2026-08-17 17:08:54 +0900

    Remove unused root battle runtime barrel

--- battle-runtime.js
+++
@@ -1,9 +0,0 @@
-// Generated browser entrypoint from private main 2858dd91e9f84aa715377c4b9fa7b76f190e94d9.
-export * from "./runtime/battle-core-battle-loop.js";
-export * from "./runtime/battle-core-command-phase.js";
-export * from "./runtime/battle-core-attack-phase.js";
-export * from "./runtime/battle-core-end-of-battle.js";
-export * from "./runtime/battle-core-end-of-round.js";
-export * from "./runtime/battle-core-turn-vertical-slice.js";
-export * from "./runtime/battle-runtime-integration.js";
-export * from "./runtime/browser-battle-round-runtime.js";
