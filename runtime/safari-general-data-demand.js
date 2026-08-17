15d1b70b7dfc7c26e2f8baa829bb2fc0f4ea987a
jolteondark <somasoso0522@gmail.com>
2026-08-17 15:23:19 +0900

    Safari: bound combat demand imports

--- runtime/safari-general-data-demand.js
+++ runtime/safari-general-data-demand.js
@@ -3,10 +3,22 @@ import {
   safariGeneralMastersInstalled,
 } from "./safari-playable-data.js";
 
+const DATA_IMPORT_TIMEOUT_MS = 60_000;
+const COMBAT_IMPORT_TIMEOUT_MS = 20_000;
 let loading = null;
 let combatLoading = null;
 let encounterRuntime = null;
 let trainerGenerator = null;
+
+function withTimeout(promise, timeoutMs, label) {
+  let timer = null;
+  const timeout = new Promise((_, reject) => {
+    timer = setTimeout(() => reject(new Error(`${label} timed out after ${timeoutMs}ms`)), timeoutMs);
+  });
+  return Promise.race([promise, timeout]).finally(() => {
+    if (timer !== null) clearTimeout(timer);
+  });
+}
 
 export function safariGeneralDataReady() {
   return safariGeneralMastersInstalled();
@@ -17,7 +29,11 @@ export async function ensureSafariGeneralData() {
     return { loaded: false, alreadyLoaded: true };
   }
   if (!loading) {
-    loading = import("./safari-general-encounter-data-loader.js")
+    loading = withTimeout(
+      import("./safari-general-encounter-data-loader.js"),
+      DATA_IMPORT_TIMEOUT_MS,
+      "Safari GENERAL data import",
+    )
       .then((data) => {
         const installed = installSafariGeneralMasters(
           data.SAFARI_GENERAL_SPECIES_MASTERS,
@@ -42,10 +58,14 @@ export async function ensureSafariGeneralCombatData() {
   if (!combatLoading) {
     combatLoading = ensureSafariGeneralData()
       .then(async (dataResult) => {
-        const [encounter, trainer] = await Promise.all([
-          import("./safari-general-encounter-runtime.js"),
-          import("./mapless-dynamic-trainer-generator.js"),
-        ]);
+        const [encounter, trainer] = await withTimeout(
+          Promise.all([
+            import("./safari-general-encounter-runtime.js"),
+            import("./mapless-dynamic-trainer-generator.js"),
+          ]),
+          COMBAT_IMPORT_TIMEOUT_MS,
+          "Safari GENERAL combat modules",
+        );
         encounterRuntime = encounter;
         trainerGenerator = trainer;
         return { ...dataResult, combatModulesLoaded: true };
