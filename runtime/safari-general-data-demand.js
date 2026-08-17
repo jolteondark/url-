6b2284735fc78f5c8881e1aad14e36ca55db1628
jolteondark <somasoso0522@gmail.com>
2026-08-17 22:37:16 +0900

    Data: remove GENERAL import timeout adapter

--- runtime/safari-general-data-demand.js
+++ runtime/safari-general-data-demand.js
@@ -3,22 +3,10 @@ import {
   safariGeneralMastersInstalled,
 } from "./safari-playable-data.js";
 
-const DATA_IMPORT_TIMEOUT_MS = 60_000;
-const COMBAT_IMPORT_TIMEOUT_MS = 20_000;
 let loading = null;
 let combatLoading = null;
 let encounterRuntime = null;
 let trainerGenerator = null;
-
-function withTimeout(promise, timeoutMs, label) {
-  let timer = null;
-  const timeout = new Promise((_, reject) => {
-    timer = setTimeout(() => reject(new Error(`${label} timed out after ${timeoutMs}ms`)), timeoutMs);
-  });
-  return Promise.race([promise, timeout]).finally(() => {
-    if (timer !== null) clearTimeout(timer);
-  });
-}
 
 export function safariGeneralDataReady() {
   return safariGeneralMastersInstalled();
@@ -29,11 +17,7 @@ export async function ensureSafariGeneralData() {
     return { loaded: false, alreadyLoaded: true };
   }
   if (!loading) {
-    loading = withTimeout(
-      import("./safari-general-encounter-data-loader.js"),
-      DATA_IMPORT_TIMEOUT_MS,
-      "Safari GENERAL data import",
-    )
+    loading = import("./safari-general-encounter-data-loader.js")
       .then((data) => {
         const installed = installSafariGeneralMasters(
           data.SAFARI_GENERAL_SPECIES_MASTERS,
@@ -58,14 +42,10 @@ export async function ensureSafariGeneralCombatData() {
   if (!combatLoading) {
     combatLoading = ensureSafariGeneralData()
       .then(async (dataResult) => {
-        const [encounter, trainer] = await withTimeout(
-          Promise.all([
-            import("./safari-general-encounter-runtime.js"),
-            import("./mapless-dynamic-trainer-generator.js"),
-          ]),
-          COMBAT_IMPORT_TIMEOUT_MS,
-          "Safari GENERAL combat modules",
-        );
+        const [encounter, trainer] = await Promise.all([
+          import("./safari-general-encounter-runtime.js"),
+          import("./mapless-dynamic-trainer-generator.js"),
+        ]);
         encounterRuntime = encounter;
         trainerGenerator = trainer;
         return { ...dataResult, combatModulesLoaded: true };
