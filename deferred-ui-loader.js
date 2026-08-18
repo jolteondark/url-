5fa2536735c6cf6812fd286b59d5afee6e3a6572
jolteondark <somasoso0522@gmail.com>
2026-08-19 01:19:17 +0900

    Safari UI: deliver Battle Bag fix through fresh loader URL (#347)
    * Safari UI: version Battle Bag menu delivery
    
    * Safari UI: refresh deferred menu delivery
    
    * Test Player: guard Battle Bag Safari delivery

--- deferred-ui-loader.js
+++ deferred-ui-loader.js
@@ -45,7 +45,7 @@ async function loadMenuUi() {
   loadStyle("./bridge-shell.css");
   loadStyle("./game-menu.css");
   await Promise.all([
-    loadModule("./game-menu-bridge.js"),
+    loadModule("./game-menu-bridge.js?v=20260819-0115"),
     loadModule("./party-panel-bridge.js"),
     loadModule("./storage-panel-bridge.js"),
     loadModule("./party-storage-controls-bridge.js"),
