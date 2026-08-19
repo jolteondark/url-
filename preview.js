d8e061a0c5b2bb3a45c4ba815e613dc25c7e97f0
jolteondark <somasoso0522@gmail.com>
2026-08-20 05:33:39 +0900

    Safari: refresh carryover focus delivery

--- preview.js
+++ preview.js
@@ -87,7 +87,7 @@ async function startPreview(action) {
   try {
     await appPromise;
     replacementPresentationPromise ??= import("./battle-player-replacement-presentation.js?v=20260820-0233");
-    carryoverPresentationPromise ??= import("./carryover-next-run-presentation.js?v=20260820-0434");
+    carryoverPresentationPromise ??= import("./carryover-next-run-presentation.js?v=20260820-0531");
     const [, carryoverPresentation] = await Promise.all([
       replacementPresentationPromise,
       carryoverPresentationPromise,
