f91231affbe324dfe7a0c9cf220dd42c29a4fb71
jolteondark <somasoso0522@gmail.com>
2026-08-20 15:32:50 +0900

    Safari: load voluntary Battle Party switch bridge

--- deferred-ui-loader.js
+++ deferred-ui-loader.js
@@ -50,6 +50,7 @@ async function loadMenuUi() {
   await Promise.all([
     loadModule("./game-menu-bridge.js?v=20260820-1234"),
     loadModule("./party-panel-bridge.js"),
+    loadModule("./battle-party-voluntary-switch-bridge.js?v=20260820-1530"),
     loadModule("./storage-panel-bridge.js"),
     loadModule("./party-storage-controls-bridge.js"),
     loadModule("./species-form-metadata-bridge.js"),
