1d013e016cf8b8ef7998cda76261836dc2c94e59
jolteondark <somasoso0522@gmail.com>
2026-08-20 12:54:01 +0900

    Safari: cache-bust missing-sprite fill bridge

--- deferred-ui-loader.js
+++ deferred-ui-loader.js
@@ -35,7 +35,7 @@ async function loadBoardPresentation() {
 
 async function loadBattleUi() {
   loadStyle("./battle-core-safety.css");
-  await loadModule("./canonical-battle-sprite-bridge.js?v=20260820-1220");
+  await loadModule("./canonical-battle-sprite-bridge.js?v=20260820-1252");
 }
 
 async function loadShopUi() {
