1470c171b2ee1c7f30361b5234c0685fe098f2e5
jolteondark <somasoso0522@gmail.com>
2026-08-20 11:23:03 +0900

    Safari: cache-bust reverted Battle sprite bridge

--- deferred-ui-loader.js
+++ deferred-ui-loader.js
@@ -35,7 +35,7 @@ async function loadBoardPresentation() {
 
 async function loadBattleUi() {
   loadStyle("./battle-core-safety.css");
-  await loadModule("./canonical-battle-sprite-bridge.js?v=20260820-1110");
+  await loadModule("./canonical-battle-sprite-bridge.js?v=20260820-1117");
 }
 
 async function loadShopUi() {
