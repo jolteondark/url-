49fc1d34a9c492043e577b7caf1bcb6936dac3e8
jolteondark <somasoso0522@gmail.com>
2026-08-21 14:33:59 +0900

    Safari: refresh replacement presentation delivery

--- preview.js
+++ preview.js
@@ -86,7 +86,7 @@ async function startPreview(action) {
   }
   try {
     await appPromise;
-    replacementPresentationPromise ??= import("./battle-player-replacement-presentation.js?v=20260820-1834");
+    replacementPresentationPromise ??= import("./battle-player-replacement-presentation.js?v=20260821-1434");
     carryoverPresentationPromise ??= import("./carryover-next-run-presentation.js?v=20260820-0531");
     const [, carryoverPresentation] = await Promise.all([
       replacementPresentationPromise,
