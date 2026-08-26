9fcb3c9c5ad2c6391b97e9b17eec6fc7af696e73
jolteondark <somasoso0522@gmail.com>
2026-08-26 17:05:40 +0900

    Wire Old Statue offer Battle under eligibility gate

--- runtime/safari-old-statue-offer-eligibility.js
+++ runtime/safari-old-statue-offer-eligibility.js
@@ -4,7 +4,7 @@ import {
   safariOldStatueOfferEntries as baseOfferEntries,
   safariOldStatuePrayNeedsPokemon,
   safariOldStatuePresentation,
-} from "./safari-old-statue-offer-treasure.js?v=20260826-1600";
+} from "./safari-old-statue-offer-battle.js?v=20260826-1700";
 import { maplessV108ItemMetadata } from "./mapless-v108-item-metadata.js";
 
 function offerableItem(id) {
