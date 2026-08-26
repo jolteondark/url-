b010c16548e954f5f9924bb9ed8a168ca40f3be6
jolteondark <somasoso0522@gmail.com>
2026-08-26 12:02:04 +0900

    Playable: route Old Statue offer reveal adapter

--- old-statue-touch-presentation.js
+++ old-statue-touch-presentation.js
@@ -4,7 +4,7 @@ import {
   safariOldStatueOfferEntries,
   safariOldStatuePrayNeedsPokemon,
   safariOldStatuePresentation,
-} from "./runtime/safari-old-statue-offer-simple.js?v=20260826-1105";
+} from "./runtime/safari-old-statue-offer-board-reveal.js?v=20260826-1200";
 import { saveSafariPlayableRun } from "./runtime/safari-web-startup.js";
 
 let resolving = false;
