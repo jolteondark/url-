94e616f08eead87a405bed89aa9229e153c1b529
jolteondark <somasoso0522@gmail.com>
2026-08-31 19:04:23 +0900

    Images: inventory published canonical Battle UI assets

--- runtime/canonical-battle-ui-sources.js
+++ runtime/canonical-battle-ui-sources.js
@@ -22,11 +22,14 @@ export const MAPLESS_BATTLE_UI_CANONICAL_RELEASE = Object.freeze({
 });
 
 export const PUBLISHED_CANONICAL_BATTLE_UI = Object.freeze({
+  'ability_bar.png': Object.freeze({ canonicalGitBlobSha: 'c2dd369fc4839df86bfab3ef00fc0e2a8574bf41', bytes: 941 }),
   'cursor_command.png': Object.freeze({
     canonicalGitBlobSha: 'c73f69b29b93355a605d7c0d2aa611e36a007020',
     canonicalSha256: '614722591ecfa667be586e0c9df985c5b4bcbac2b8f3c3b2beed1fc4c7094d39',
     bytes: 8456,
   }),
+  'cursor_mega.png': Object.freeze({ canonicalGitBlobSha: '0dafea4f5876c72a0f26eec9515832e1a1168636', bytes: 2131 }),
+  'cursor_shift.png': Object.freeze({ canonicalGitBlobSha: '6045bfe396f7abe54ee48959a9c2589cece403c7', bytes: 608 }),
   'databox_normal.png': Object.freeze({
     canonicalGitBlobSha: 'fcc043f814f25e8345ac7b51e5f7573d2fa60d6d',
     bytes: 938,
@@ -35,27 +38,42 @@ export const PUBLISHED_CANONICAL_BATTLE_UI = Object.freeze({
     canonicalGitBlobSha: '5d223b28161ed851ad12602c02058f773d7bec03',
     bytes: 783,
   }),
-  'overlay_message.png': Object.freeze({
-    canonicalGitBlobSha: 'aa8257b992a407256968484fc7b35f2e6ab8bb60',
-    bytes: 947,
+  'databox_safari.png': Object.freeze({ canonicalGitBlobSha: '5d85afa5b9af509632d664c856799ef5a4b93567', bytes: 1102 }),
+  'databox_thin.png': Object.freeze({ canonicalGitBlobSha: 'cdfe85b98f31aa4c68884d267e99193fd6f4d74a', bytes: 756 }),
+  'databox_thin_foe.png': Object.freeze({ canonicalGitBlobSha: '0c3a586cf977e31a3bc5cd68460ede0f4c11086a', bytes: 783 }),
+  'icon_ball.png': Object.freeze({ canonicalGitBlobSha: '093721a9f94548d1c9128f6c68259735bb4753c2', bytes: 510 }),
+  'icon_ball_empty.png': Object.freeze({ canonicalGitBlobSha: '5d784db19b91af9a9c63c0ad941aeb2e620bd371', bytes: 385 }),
+  'icon_ball_faint.png': Object.freeze({ canonicalGitBlobSha: 'b98a4c7410d390a84d022b7abbe3e5dfa9994378', bytes: 502 }),
+  'icon_ball_status.png': Object.freeze({ canonicalGitBlobSha: '06d84406bbcd0f33b9287855e37a0664d0f138f7', bytes: 496 }),
+  'icon_mega.png': Object.freeze({ canonicalGitBlobSha: 'aaaf0b98663d23612f7ae9f8e420df58dae40392', bytes: 556 }),
+  'icon_numbers.png': Object.freeze({ canonicalGitBlobSha: 'f7bfb20dbf580186b3674794203c7ecafa6330b1', bytes: 639 }),
+  'icon_own.png': Object.freeze({ canonicalGitBlobSha: '33975e3b2c63dc00ebe1c386a77d927248800ba9', bytes: 390 }),
+  'icon_primal_Groudon.png': Object.freeze({ canonicalGitBlobSha: '683eae62243c2d9be2cd6918f0b299e94f5c85ee', bytes: 645 }),
+  'icon_primal_Kyogre.png': Object.freeze({ canonicalGitBlobSha: '6f7d3592b5f801df40f463881d3277ecafaf6eb0', bytes: 650 }),
+  'icon_statuses.png': Object.freeze({
+    canonicalGitBlobSha: 'b3e204c5d53a6d17a556a8602f516ea4db2bbfff',
+    canonicalSha256: 'f1220f895c686dc8601916769be7853960a44f0efdccbd4b1ec429906dd33fa0',
+    bytes: 756,
   }),
+  'judgment.png': Object.freeze({ canonicalGitBlobSha: '0d43454e9784ef60048e839262c057d82d1696dd', bytes: 681 }),
+  'overlay_exp.png': Object.freeze({ canonicalGitBlobSha: '025c43b59ed4a55f7564296b54742b2a4dd02ac2', bytes: 92 }),
   'overlay_fight.png': Object.freeze({
     canonicalGitBlobSha: 'd61ef321964b693e31bca81a34a6fd0dba5e5a60',
     bytes: 1602,
-  }),
-  'overlay_lv.png': Object.freeze({
-    canonicalGitBlobSha: '758fccbb3d776f6731fa2f7600aee4913a37d515',
-    bytes: 328,
   }),
   'overlay_hp.png': Object.freeze({
     canonicalGitBlobSha: '7064a0ca0dc20a7fd91c4f155af93d95b58c8280',
     canonicalSha256: '087d8f80277526e7814a965dfdc27c61312f5e29a1bd5632bfea8e280d2b72b5',
     bytes: 120,
   }),
-  'icon_statuses.png': Object.freeze({
-    canonicalGitBlobSha: 'b3e204c5d53a6d17a556a8602f516ea4db2bbfff',
-    canonicalSha256: 'f1220f895c686dc8601916769be7853960a44f0efdccbd4b1ec429906dd33fa0',
-    bytes: 756,
+  'overlay_lineup.png': Object.freeze({ canonicalGitBlobSha: '2cb2a57d47d003d1b4aed5baebb0f691a458dd43', bytes: 328 }),
+  'overlay_lv.png': Object.freeze({
+    canonicalGitBlobSha: '758fccbb3d776f6731fa2f7600aee4913a37d515',
+    bytes: 328,
+  }),
+  'overlay_message.png': Object.freeze({
+    canonicalGitBlobSha: 'aa8257b992a407256968484fc7b35f2e6ab8bb60',
+    bytes: 947,
   }),
   'types.png': Object.freeze({
     canonicalGitBlobSha: '7f1b9d801436bbf1fc215aed24093292d3c0c8ba',
