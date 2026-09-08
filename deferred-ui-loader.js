a10f97905bb12c8192090b1ae9587e01aa17618d
jolteondark <somasoso0522@gmail.com>
2026-09-09 00:08:00 +0900

    Presentation: refresh Battle resolver Safari generation

--- deferred-ui-loader.js
+++ deferred-ui-loader.js
@@ -6,7 +6,7 @@ let boardPresentationManifestPromise = null;
 let battleChromePromise = null;
 let trainerBattlePresentationPromise = null;
 
-const BATTLE_PRESENTATION_PUBLIC_REVISION = "20260908-1900";
+const BATTLE_PRESENTATION_PUBLIC_REVISION = "20260909-0015";
 const battlePresentationUrl = (path) => `${path}?v=${BATTLE_PRESENTATION_PUBLIC_REVISION}`;
 const MENU_SPRITE_PRESENTATION_PUBLIC_REVISION = "20260902-1800";
 
