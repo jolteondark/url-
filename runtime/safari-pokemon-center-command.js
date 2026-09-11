54e0c81107de24b8d16a245b430bd83a377ed9bd
jolteondark <somasoso0522@gmail.com>
2026-09-11 14:29:37 +0900

    Playable: dispatch Evolution Lab from Day Board

--- runtime/safari-pokemon-center-command.js
+++ runtime/safari-pokemon-center-command.js
@@ -13,6 +13,7 @@ import { interactiveSafariHotSpring } from "./safari-hot-spring-interaction.js";
 import { interactiveSafariFakeNurse } from "./safari-fake-nurse-interaction.js";
 import { interactiveSafariTravelingCook } from "./safari-traveling-cook-interaction.js";
 import { interactiveSafariFloodedRiver } from "./safari-flooded-river-interaction.js";
+import { interactiveSafariEvolutionLab } from "./safari-evolution-lab-interaction.js";
 import { interactiveSafariBuriedItem } from "./safari-buried-item-interaction.js";
 import { interactiveSafariEggShop } from "./safari-egg-shop-interaction.js";
 import { openSafariTreasureTouch } from "./safari-treasure-chest-interaction.js";
@@ -102,6 +103,7 @@ export function activateSafariDayBoardCell(runtime, index) {
     if (event.normal_event_id === "item_collector" && typeof globalThis.document !== "undefined") return openSafariItemCollectorTouch(runtime, index);
     if (event.normal_event_id === "treasure_map_seller") return interactiveSafariTreasureMapSeller(runtime, index);
     if (event.normal_event_id === "treasure_map_result") return activateSafariTreasureMapResult(runtime, index);
+    if (event.normal_event_id === "evolution_lab") return interactiveSafariEvolutionLab(runtime, index);
     if (typeof globalThis.document !== "undefined" && supportsSafariNormalEventTouch(event.normal_event_id)) return openSafariNormalEventTouch(runtime, index);
     if (event.normal_event_id === "street_performer") return interactiveSafariStreetPerformer(runtime, index);
     if (event.normal_event_id === "mushroom_field") return interactiveSafariMushroomField(runtime, index);
